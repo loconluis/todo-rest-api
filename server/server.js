743472ae65f1da8ba47e36632cfc094cd31140d9
loconluis <luis.locon@gmail.com>
2017-10-18 01:32:25 -0600

    Add POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,29 @@
-const { mongoose } = require('./db/mongoose')
+// Server modules
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const { Todo } = require('./models/todo.model')
+// Dependencies
+const { mongoose } = require('./db/mongoose') // connection instance
+const { Todo } = require('./models/todo.model') // Model of ToDo
+const { User } = require('./models/user.model') // Model of User
+// App instance of express
+const app = express()
+// middleware of bodyparser
+app.use(bodyParser.json())
 
-const { User } = require('./models/user.model')
+app.post('/todos', (req, res) => {
+  let todo = new Todo({
+    text: req.body.text
+  })
+
+  todo.save()
+    .then(result => res.send(result))
+    .catch(err => res.status(400).send(err))
+
+  // console.log(req.body)
+})
+
+// Running port
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
