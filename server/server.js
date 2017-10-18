8ddf4aa95cd3d440aef693530ac1576301cb9d23
loconluis <luis.locon@gmail.com>
2017-10-18 16:51:32 -0600

    Adding GET /todo/:id route to get an specific todo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Server modules
 const express = require('express')
 const bodyParser = require('body-parser')
+const { ObjectID } = require('mongodb')
 
 // Dependencies
 const { mongoose } = require('./db/mongoose') // connection instance
@@ -10,7 +11,7 @@ const { User } = require('./models/user.model') // Model of User
 const app = express()
 // middleware of bodyparser
 app.use(bodyParser.json())
-
+// Post a new todo on db
 app.post('/todos', (req, res) => {
   let todo = new Todo({
     text: req.body.text
@@ -20,14 +21,29 @@ app.post('/todos', (req, res) => {
     .then(result => res.send(result))
     .catch(err => res.status(400).send(err))
 })
-
+// Get all todos on db
 app.get('/todos', (req, res) => {
   // getting todos
   Todo.find()
     .then((todos) => res.send({todos}))
     .catch(err => res.status(400).send(err))
 })
+// get an specific todo with ID
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
 
+  if (!ObjectID(id)) { return res.status(404).send() }
+
+  // if ID is valid then search
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send({message: 'NOT RESULTS'})
+      }
+      res.status(200).send({todo})
+    })
+    .catch(e => res.status(400).send({message: 'Fail request' + e}))
+})
 // Running port
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
