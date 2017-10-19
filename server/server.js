e678df60222d7e1fe6ae368c2c17bcc2f3ecf062
loconluis <luis.locon@gmail.com>
2017-10-18 21:13:37 -0600

    Adding DEL /todos/:id route to delete Todos

--- server/server.js
+++ server/server.js
@@ -45,6 +45,24 @@ app.get('/todos/:id', (req, res) => {
     })
     .catch(e => res.status(400).send({message: 'Fail request' + e}))
 })
+// delete an specific todo with ID
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send({message: 'NOT VALID ID'})
+  }
+
+  Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send({message: 'NOT RESULTS'})
+      }
+
+      res.status(200).send({todo})
+    })
+    .catch(e => res.status(400).send({message: 'Fail request ' + e}))
+})
 // Running port
 app.listen(port, () => {
   console.log(`Server is running on ${port}`)
