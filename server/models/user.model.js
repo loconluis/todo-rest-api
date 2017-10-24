aabfd18c0a90752b99fad639a81f1874cb6a0ed5
loconluis <luis.locon@gmail.com>
2017-10-23 22:35:51 -0600

    Update config to use JSON file

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -47,7 +47,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
   // this scope take a Schema
   let access = 'auth'
-  let token = jwt.sign({_id: this._id.toHexString(), access}, 'abc123').toString()
+  let token = jwt.sign({_id: this._id.toHexString(), access}, process.env.JWT_SECRET).toString()
 
   this.tokens.push({access, token})
 
@@ -66,7 +66,7 @@ UserSchema.statics.findByToken = function (token) {
   let decoded
 
   try {
-    decoded = jwt.verify(token, 'abc123')
+    decoded = jwt.verify(token, process.env.JWT_SECRET)
   } catch (e) {
     return Promise.reject()
   }
